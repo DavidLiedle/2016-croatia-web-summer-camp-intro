9550a807b28ded5a6b1a8c91c2ef6f4580fa2e54
David Christian Liedle <david.liedle@gmail.com>
2016-08-25 17:06:31 -0400

    Database initialization

--- installation/run.sh
+++ installation/run.sh
@@ -10,5 +10,10 @@ echo "
 |  __// /_  |  __/| | (_| | |_|  _| (_) | |  | | | | | |
  \___/____| |_|   |_|\__,_|\__|_|  \___/|_|  |_| |_| |_|
 "
-echo "All the things!"
+echo -n "Running composer installation..."
 composer install -n
+echo "..DONE!"
+echo "Obtaining the initial database..."
+php -d memory_limit=-1 app/console ezplatform:install --env prod clean
+echo "..DONE!"
+echo
